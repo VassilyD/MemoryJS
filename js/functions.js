4e90dec277c6939d367422f8a3aa84fd6568dc89
Vassily Dubois <vassily.dubois@gmail.com>
2017-09-07 04:56:26 +0200

    ajout gestion clic, check double, score et victoire

--- js/functions.js
+++ js/functions.js
@@ -1,8 +1,9 @@
 const IMG_SRC = 'img/card';
 let game;
 
-let Card = function(imgSrc, idcard) {
+let Card = function(imgSrc, idcard, classCard) {
 	this.idDom = idcard;
+	this.class = classCard;
 	this.dom = this.imgBack = this.imgUp = null;
 	this.up = false;
 	this.imgUpSrc = imgSrc;
@@ -19,20 +20,28 @@ let Card = function(imgSrc, idcard) {
 		else {
 			this.dom = document.createElement('div');
 			this.dom.id = this.idDom;
-			this.dom.classList.add('cards');
-			this.dom.onclick = function(){game.cards[Number(this.id.slice(4))].flip()};
+			this.dom.classList.add('cards', this.class);
+			this.dom.setAttribute('data', 0);
+			this.dom.onclick = function(){game.cards[Number(this.getAttribute('data'))].flip()};
 		}
 	}
 
 	this.flip = function() {
-		if(!this.up) {
-			this.up = true;
-			this.imgBack.setAttribute('hidden', true);
-			this.imgUp.removeAttribute('hidden');
+		if(game.getNbFlip() < 2) {
+			if(!this.up) {
+				this.up = true;
+				this.imgBack.setAttribute('hidden', true);
+				this.imgUp.removeAttribute('hidden');
+				game.addFlipped(this);
+			}
 		} else {
-			this.up = false;
-			this.imgUp.setAttribute('hidden', true);
-			this.imgBack.removeAttribute('hidden');
+			//if(!this.up) {
+				game.unFlip();
+				this.up = true;
+				this.imgBack.setAttribute('hidden', true);
+				this.imgUp.removeAttribute('hidden');
+				game.addFlipped(this);
+			//}
 		}
 	}
 
@@ -69,18 +78,76 @@ let Card = function(imgSrc, idcard) {
 	this.setImgUp(this.imgUpSrc);
 }
 
-let Memory = function(nblines = 4, nbCols = 4, idCards = 'cardsZone') {
+let Memory = function(nbline = 4, nbCol = 4, idCards = 'cardsZone') {
 	this.cards = [];
-	this.nbCards = nblines * nbCols;
+	this.nbCards = nbline * nbCol;
+	this.nbCols = nbCol;
 	this.idDomCards = idCards;
 	this.domCards = document.createElement('div');
 	this.domCards.id = idCards;
-	this.domCards.setAttribute('style', '--nbLines: ' + nblines + '; --nbCols: ' + nbCols);
-	for(i = 0; i < this.nbCards; i++) {
-		var tmp = new Card((IMG_SRC + ('00' + (i + 1)).slice(-2) + '.svg'), 'card' + i);
-		this.cards.push(tmp);
-		this.domCards.appendChild(this.cards[this.cards.length - 1].getDom());
+	this.domCards.setAttribute('style', '--nbLines: ' + nbline + '; --nbCols: ' + nbCol);
+	this.flipped = [];
+	this.score = 0;
+
+	this.getNbFlip = function() {
+		return this.flipped.length;
 	}
+
+	this.addFlipped = function(card) {
+		this.flipped.push(card);
+		if(this.getNbFlip() > 1) this.checkSameCard();
+	}
+
+	this.checkSameCard = function() {
+		if(this.flipped[0].getDom().classList[1] == this.flipped[1].getDom().classList[1]) {
+			this.score++;
+			document.getElementsByTagName('h1')[0].innerHTML = 'Memory JS : score = ' + this.score;
+			this.flipped[0].dom.onclick = '';
+			this.flipped[1].dom.onclick = '';
+			this.flipped = [];
+			if(this.score == this.nbCards / 2) alert('Vous avez gagné');
+			//this.flipped[0].imgUp.setAttribute('hidden', true);
+			//this.flipped[0].imgBack.setAttribute('hidden', true);
+			//this.flipped[1].imgUp.setAttribute('hidden', true);
+			//this.flipped[1].imgBack.setAttribute('hidden', true);
+		}
+	}
+
+	this.unFlip = function() {
+		this.flipped[0].imgUp.setAttribute('hidden', true);
+		this.flipped[0].imgBack.removeAttribute('hidden');
+		this.flipped[0].up = false;
+		this.flipped[1].imgUp.setAttribute('hidden', true);
+		this.flipped[1].imgBack.removeAttribute('hidden');
+		this.flipped[1].up = false;
+		this.flipped = [];
+	}
+
+	// création des cartes
+	for(i = 0; i < this.nbCards; i++) {
+		var tmp = new Card((IMG_SRC + ('00' + (Math.floor(i / 2))).slice(-2) + '.svg'), 'card' + i, 'card' + (Math.floor(i / 2)));
+		this.cards.push(tmp);
+	}
+
+	// Mémange des cartes
+	for(i = 0; i < this.nbCards; i++) {
+		var newI = Math.floor(Math.random() * this.nbCards);
+		var tmp = this.cards[i];
+		this.cards[i] = this.cards[newI];
+		this.cards[i].getDom().setAttribute('data', i);
+		this.cards[newI] = tmp;
+		this.cards[newI].getDom().setAttribute('data', newI);
+	}
+	
+	// Ajout des cartes au DOM
+	for(i = 0; i < this.nbCards; i++) {
+		this.domCards.appendChild(this.cards[i].getDom());
+		if((i + 1) % this.nbCols == 0) {
+			var tmp = document.createElement('br');
+			this.domCards.appendChild(tmp);
+		}
+	}
+
 	var zone = document.getElementById('cardsZone');
 	zone.parentNode.replaceChild(this.domCards, zone);
 }
