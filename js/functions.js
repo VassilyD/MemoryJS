574584791873d4d791e5cbe78b60b97700ae9fdf
Vassily <vassily.dubois@gmail.com>
2017-09-09 07:33:18 +0200

    Update functions.js
    oublie changement attribut du DOM

--- js/functions.js
+++ js/functions.js
@@ -138,6 +138,7 @@ let Memory = function(nbline = 4, nbCol = 4, idCards = 'cardsZone') {
 		var tmpCards = [];
 		for(i = 0; i < this.nbCards; i++) {
 			var newI = Math.floor(Math.random() * this.cards.length);
+			this.cards[newI].getDom().setAttribute('data', i);
 			tmpCards.push(this.cards[newI]);
 			this.cards = this.cards.slice(0, newI).concat(this.cards.slice(newI + 1));
 			/*var tmp = this.cards[i];
